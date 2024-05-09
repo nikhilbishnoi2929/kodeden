c781cf833f21fd6d70bddb0f5f9d1e9b657a8190
nikhil bishnoi <nikhilbishnoibhadu@gmail.com>
2024-05-09 15:37:20 +0530

    page done

--- assest/js/main.js
+++ assest/js/main.js
@@ -70,3 +70,17 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("load", function () {
+    setTimeout(function () {
+      let loadingOverlay = document.getElementById("loadingOverlay");
+      loadingOverlay.style.display = "none";
+      document.querySelector("body").classList.remove("overflow_hidden");
+    }, 3000);
+  });
+});
